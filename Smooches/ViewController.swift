b6882269b077aeac54840f5b315beb66a5fa9233
Marwan Khalid <marwankhalid44@gmail.com>
2022-11-21 15:22:10 +0500

    Initial Commit

---
+++ Smooches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
