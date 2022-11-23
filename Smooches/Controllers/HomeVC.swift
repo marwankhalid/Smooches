4d5400e007db02d1530006c63c9064b9e9d631fe
Marwan Khalid <marwankhalid44@gmail.com>
2022-11-23 18:14:50 +0500

    updated

---
+++ Smooches/Controllers/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 11/23/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
---
+++ Smooches/Views/HomeTVC.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTVC.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 11/23/22.
+//
+
+import UIKit
+
+class HomeTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
