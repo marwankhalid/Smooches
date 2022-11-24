95bb3777bc9118ac877aba9e76119865472cc667
Marwan Khalid <marwankhalid44@gmail.com>
2022-11-24 12:46:55 +0500

    updated

--- Smooches/Controllers/AlertVC.swift
+++ Smooches/Controllers/AlertVC.swift
@@ -9,8 +9,42 @@ import UIKit
 
 class AlertVC: UIViewController {
     
+    @IBOutlet weak var contentViewBase: UIView!
+    
+    @IBOutlet weak var scrollViewS: UIScrollView!
+    
+    @IBOutlet weak var scrollViewContentView: UIView!
+    
+    @IBOutlet weak var scrollViewContentViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var startTimeT: UITextField!
+    
+    @IBOutlet weak var endTimeT: UITextField!
+    
+    @IBOutlet weak var reminderTypeT: UITextField!
+    
+    @IBOutlet weak var addB: UIButton!
+    
+    @IBOutlet weak var addContactsTableView: UITableView!
+    
+    @IBOutlet weak var addContactsTableViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var messageT: UITextView!
+    
+    @IBOutlet weak var submitB: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func addB(_ sender: Any) {
+    }
+    
+    @IBAction func submitB(_ sender: Any) {
+    }
+    
 }
---
+++ Smooches/Views/AlertTVC.swift
@@ -0,0 +1,32 @@
+//
+//  AlertTVC.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 11/24/22.
+//
+
+import UIKit
+
+class AlertTVC: UITableViewCell {
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var nameL: UILabel!
+    @IBOutlet weak var checkB: UIButton!
+    
+    @IBAction func checkB(_ sender: Any) {
+        
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
