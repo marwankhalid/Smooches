6697dfc24a33d05909a36e1d1e2f4881424717cd
Marwan Khalid <marwankhalid44@gmail.com>
2022-11-24 14:22:32 +0500

    updated

--- Smooches/Controllers/AlertVC.swift
+++ Smooches/Controllers/AlertVC.swift
@@ -42,7 +42,21 @@ class AlertVC: UIViewController {
         setupTableView()
         setHieghts()
         setupViews()
-        
+        setupTextView()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        //scrollViewS.scrollToBottom(animated: true)
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
     }
     
     private func setupViews(){
@@ -55,12 +69,37 @@ class AlertVC: UIViewController {
         addB.layer.cornerRadius = addB.bounds.height / 2
         contentViewBase.layer.cornerRadius = 20
         scrollViewS.layer.cornerRadius = 20
+        
+    }
+    
+    private func setupTextView(){
+        messageT.text = """
+                                Type Message Here
+                                """
+        messageT.layer.cornerRadius = 20
+        messageT.layer.borderWidth = 1
+        messageT.layer.borderColor = UIColor.link.cgColor
+        messageT.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        messageT.textContainer.lineFragmentPadding = 20
+        messageT.textColor = UIColor.lightGray
+        messageT.font = UIFont.systemFont(ofSize: 16)
+        messageT.returnKeyType = .done
+        messageT.delegate = self
+        messageT.autocorrectionType = .no
+        messageT.autocapitalizationType = .none
+        messageT.layer.cornerRadius = 20
+        messageT.isEditable = true
     }
     
     private func setHieghts(){
         let newHeight = CGFloat((dataSource.count * 80) + 20)
         tableViewHeight.constant = newHeight
         scrollViewContentViewHeight.constant = (scrollViewContentViewHeight.constant - 150) + newHeight
+        
+        let addContactsTableViewHeightNewHiehgt = CGFloat((3 * 70))
+        
+        addContactsTableViewHeight.constant = addContactsTableViewHeightNewHiehgt
+        scrollViewContentViewHeight.constant = (scrollViewContentViewHeight.constant + addContactsTableViewHeightNewHiehgt)
     }
     
     @IBAction func addB(_ sender: Any) {
@@ -80,22 +119,92 @@ extension AlertVC:UITableViewDelegate,UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = 80
+        addContactsTableView.delegate = self
+        addContactsTableView.dataSource = self
+        addContactsTableView.rowHeight = 70
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
+        if tableView == self.tableView {
+            return dataSource.count
+        }
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AlertTVC", for: indexPath) as! AlertTVC
-        cell.checkB.setTitle("", for: .normal)
-        cell.cardView.backgroundColor = .white
-        cell.cardView.layer.cornerRadius = 10.0
-        cell.cardView.layer.shadowColor = UIColor.gray.cgColor
-        cell.cardView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        cell.cardView.layer.shadowRadius = 1.0
-        cell.cardView.layer.shadowOpacity = 0.7
+        if tableView == self.tableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AlertTVC", for: indexPath) as! AlertTVC
+            cell.checkB.setTitle("", for: .normal)
+            cell.cardView.backgroundColor = .white
+            cell.cardView.layer.cornerRadius = 10.0
+            cell.cardView.layer.shadowColor = UIColor.gray.cgColor
+            cell.cardView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+            cell.cardView.layer.shadowRadius = 1.0
+            cell.cardView.layer.shadowOpacity = 0.7
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTVC", for: indexPath) as! HomeTVC
+        cell.imgI.layer.cornerRadius = cell.imgI.bounds.height / 2
+        cell.cardV.backgroundColor = .white
+        cell.cardV.layer.cornerRadius = 10.0
+        cell.cardV.layer.shadowColor = UIColor.gray.cgColor
+        cell.cardV.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        cell.cardV.layer.shadowRadius = 1.0
+        cell.cardV.layer.shadowOpacity = 0.7
         return cell
     }
+}
+
+
+extension AlertVC:UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        //return textView.text.count + (text.count - range.length) <= maxLenghth
+        return true
+    }
     
+    func textViewDidChange(_ textView: UITextView) {
+        //wordCounterL.text = "\(textView.text.count)/300"
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = """
+                                    Type Message Here
+                                    """
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+//
+//    func textViewDidChangeSelection(_ textView: UITextView) {
+//        if textView.hasText {
+//            finishB.isEnabled = true
+//            finishB.titleLabel?.textColor = UIColor.link
+//        }
+//
+//        if !textView.hasText {
+//            finishB.isEnabled = false
+//            finishB.titleLabel?.textColor = UIColor.lightGray
+//        }
+//    }
+    
+}
+
+extension UIScrollView {
+   func scrollToBottom(animated: Bool) {
+     if self.contentSize.height < self.bounds.size.height { return }
+     let bottomOffset = CGPoint(x: 0, y: self.contentSize.height - self.bounds.size.height)
+     self.setContentOffset(bottomOffset, animated: animated)
+  }
 }
--- Smooches/Controllers/SelectContactsVC.swift
+++ Smooches/Controllers/SelectContactsVC.swift
@@ -16,20 +16,32 @@ class SelectContactsVC: UIViewController {
     @IBOutlet weak var dismissB: UIButton!
     @IBOutlet weak var pleaseSelectDescriptionL: UILabel!
     
+    var selectContactsCounter = 1
+    
+    var indexSaved = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        doneB.setTitle("", for: .normal)
-        dismissB.setTitle("", for: .normal)
+        doneB.setTitle("Done", for: .normal)
+        dismissB.setTitle("Reset", for: .normal)
         setupTableView()
     }
 
     @IBAction func doneB(_ sender: Any) {
-        
+        self.dismiss(animated: true)
     }
     
     @IBAction func dismissB(_ sender: Any) {
-        self.dismiss(animated: true)
+        setupTableView()
+        self.selectContactsCounter = 1
+        self.indexSaved.removeAll()
+        for i in 0...7 {
+            let indexpath = IndexPath(row: i, section: 0)
+            let cell = tableView.cellForRow(at: indexpath) as! HomeTVC
+            cell.imgI.alpha = 1
+        }
+        tableView.reloadData()
     }
     
 }
@@ -43,11 +55,12 @@ extension SelectContactsVC:UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 8
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTVC", for: indexPath) as! HomeTVC
+        cell.selectionStyle = .none
         cell.imgI.layer.cornerRadius = cell.imgI.bounds.height / 2
         cell.cardV.backgroundColor = .white
         cell.cardV.layer.cornerRadius = 10.0
@@ -59,7 +72,38 @@ extension SelectContactsVC:UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(123)
+        if self.selectContactsCounter <= 5 {
+            let cell = tableView.cellForRow(at: indexPath) as! HomeTVC
+            if indexSaved.count == 0 {
+                print("A")
+                let cell = tableView.cellForRow(at: indexPath) as! HomeTVC
+                cell.imgI.alpha = 0.6
+                self.selectContactsCounter += 1
+                self.indexSaved.append(indexPath.row)
+            }else if self.indexSaved.count > 0{
+                print("B")
+                for i in 0..<indexSaved.count {
+                    if indexPath.row == indexSaved[i] {
+                        print("C")
+                        print(indexPath.row)
+                        print(i)
+                        print(indexSaved[i])
+                        cell.imgI.alpha = 1
+                        self.selectContactsCounter -= 1
+                        self.indexSaved.remove(at: i)
+                        break
+                    }else {
+                        cell.imgI.alpha = 0.6
+                        self.selectContactsCounter += 1
+                        self.indexSaved.append(indexPath.row)
+                        break
+                    }
+                }
+            }
+            print(indexSaved)
+        }else {
+            print("Selection Full")
+        }
     }
     
 }
