26c2eb41f3e406cd08953a07a24c094c01b6a7a9
Marwan Khalid <marwankhalid44@gmail.com>
2022-12-07 20:22:17 +0500

    updated

---
+++ Smooches/Views/MessageTVC.swift
@@ -0,0 +1,22 @@
+//
+//  MessageTVC.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 12/7/22.
+//
+
+import UIKit
+
+class MessageTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
