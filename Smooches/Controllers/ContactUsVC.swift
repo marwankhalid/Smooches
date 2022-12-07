c47171f3cd8f96a449a488ec30596520084a07bb
Marwan Khalid <marwankhalid44@gmail.com>
2022-12-07 23:32:48 +0500

    updated

--- Smooches/Controllers/ContactUsVC.swift
+++ Smooches/Controllers/ContactUsVC.swift
@@ -27,7 +27,7 @@ class ContactUsVC: UIViewController {
         nameT.layer.borderWidth = 1.0
         nameT.layer.borderColor = UIColor.link.cgColor
         nameT.layer.cornerRadius = nameT.bounds.height / 2
-        nameT.backgroundColor = .white
+        nameT.backgroundColor = .secondarySystemBackground
         nameT.attributedPlaceholder = NSAttributedString(
             string: "Name",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
@@ -40,7 +40,7 @@ class ContactUsVC: UIViewController {
         emailT.layer.borderWidth = 1.0
         emailT.layer.borderColor = UIColor.link.cgColor
         emailT.layer.cornerRadius = emailT.bounds.height / 2
-        emailT.backgroundColor = .white
+        emailT.backgroundColor = .secondarySystemBackground
         emailT.attributedPlaceholder = NSAttributedString(
             string: "Email",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
@@ -48,6 +48,24 @@ class ContactUsVC: UIViewController {
         emailT.setLeftPaddingPoints(20)
         emailT.setRightPaddingPoints(20)
         
+        messageT.text = ""
+        messageT.backgroundColor = .secondarySystemBackground
+        messageT.layer.borderWidth = 1.0
+        messageT.layer.borderColor = UIColor.link.cgColor
+        messageT.layer.cornerRadius = messageT.bounds.height / 2
+        messageT.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        messageT.textContainer.lineFragmentPadding = 20
+        messageT.textColor = UIColor.black
+        messageT.font = UIFont.systemFont(ofSize: 16)
+        messageT.returnKeyType = .done
+        messageT.delegate = self
+        messageT.autocorrectionType = .no
+        messageT.autocapitalizationType = .none
+        messageT.layer.cornerRadius = 20
+        messageT.isEditable = true
+        
+        
+        submitB.layer.cornerRadius = submitB.bounds.height / 2
         
     }
     
@@ -58,10 +76,22 @@ class ContactUsVC: UIViewController {
     
 }
 
-extension ContactUsVC:UITextFieldDelegate {
+extension ContactUsVC:UITextFieldDelegate, UITextViewDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+        if textField == nameT {
+            emailT.becomeFirstResponder()
+        }else if textField == emailT {
+            messageT.becomeFirstResponder()
+        }
         return true
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return false
     }
 }
 
