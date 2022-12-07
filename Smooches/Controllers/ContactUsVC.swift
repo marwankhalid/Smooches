159060c694333c851e8c177177c7194db855e16d
Marwan Khalid <marwankhalid44@gmail.com>
2022-12-07 23:22:45 +0500

    updated

--- Smooches/Controllers/ContactUsVC.swift
+++ Smooches/Controllers/ContactUsVC.swift
@@ -17,10 +17,63 @@ class ContactUsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
+        
     }
+    
+    private func setupViews(){
+        nameT.delegate = self
+        nameT.borderStyle = .none
+        nameT.layer.borderWidth = 1.0
+        nameT.layer.borderColor = UIColor.link.cgColor
+        nameT.layer.cornerRadius = nameT.bounds.height / 2
+        nameT.backgroundColor = .white
+        nameT.attributedPlaceholder = NSAttributedString(
+            string: "Name",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
+        )
+        nameT.setLeftPaddingPoints(20)
+        nameT.setRightPaddingPoints(20)
+        
+        emailT.delegate = self
+        emailT.borderStyle = .none
+        emailT.layer.borderWidth = 1.0
+        emailT.layer.borderColor = UIColor.link.cgColor
+        emailT.layer.cornerRadius = emailT.bounds.height / 2
+        emailT.backgroundColor = .white
+        emailT.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
+        )
+        emailT.setLeftPaddingPoints(20)
+        emailT.setRightPaddingPoints(20)
+        
+        
+    }
+    
     
     @IBAction func submitB(_ sender: Any) {
         
     }
     
+}
+
+extension ContactUsVC:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
 }
