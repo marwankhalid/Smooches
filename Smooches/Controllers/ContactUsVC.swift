01397928e52967841b57182d95ff57f1b7053e04
Marwan Khalid <marwankhalid44@gmail.com>
2022-12-07 23:19:04 +0500

    updated

---
+++ Smooches/Controllers/ContactUsVC.swift
@@ -0,0 +1,26 @@
+//
+//  ContactUsVC.swift
+//  Smooches
+//
+//  Created by Marwan Khalid on 12/7/22.
+//
+
+import UIKit
+
+class ContactUsVC: UIViewController {
+
+    @IBOutlet weak var nameT: UITextField!
+    @IBOutlet weak var emailT: UITextField!
+    @IBOutlet weak var messageT: UITextView!
+    @IBOutlet weak var submitB: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func submitB(_ sender: Any) {
+        
+    }
+    
+}
