1e8c46921fb7f4497190d523eed87012b9988409
Marwan Khalid <marwankhalid44@gmail.com>
2022-12-08 22:29:07 +0500

    updated

--- Smooches/Controllers/AlertVC.swift
+++ Smooches/Controllers/AlertVC.swift
@@ -38,9 +38,12 @@ class AlertVC: UIViewController {
         setHieghts()
         setupViews()
         setupTextView()
+        notification()
+    }
+    
+    private func notification(){
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
